1459e10653d14e632c9e2df713762353aad43b45
Byteflux <byte@byteflux.net>
2015-03-09 00:15:20 -0700

    Fix NPC skull owner in PlayerHeads instead of cancelling the drop

--- CombatTagPlus/src/main/java/net/minelink/ctplus/PlayerHeadsListener.java
+++ CombatTagPlus/src/main/java/net/minelink/ctplus/PlayerHeadsListener.java
@@ -1,8 +1,13 @@
 package net.minelink.ctplus;
 
+import net.minelink.ctplus.compat.api.NpcIdentity;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.shininet.bukkit.playerheads.events.PlayerDropHeadEvent;
 
 public class PlayerHeadsListener implements Listener {
@@ -13,11 +18,26 @@ public class PlayerHeadsListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void discardNpcHeads(PlayerDropHeadEvent event) {
-        if (plugin.getNpcPlayerHelper().isNpc(event.getEntity())) {
-            event.setCancelled(true);
-        }
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void setOwner(PlayerDropHeadEvent event) {
+        Player player = event.getEntity();
+
+        // Do nothing if player is not a NPC
+        if (!plugin.getNpcPlayerHelper().isNpc(player)) return;
+
+        // Get NPC identity
+        NpcIdentity identity = plugin.getNpcPlayerHelper().getIdentity(player);
+        if (identity == null) return;
+
+        ItemStack item = event.getDrop();
+        item.setType(Material.SKULL_ITEM);
+
+        // Set skull owner to actual player's name
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        meta.setOwner(identity.getName());
+
+        // Save meta
+        item.setItemMeta(meta);
     }
 
 }

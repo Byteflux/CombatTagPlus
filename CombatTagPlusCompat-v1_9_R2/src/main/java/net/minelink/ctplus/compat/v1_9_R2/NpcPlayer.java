9ae862aa2ae28df06c204561b7b3f1bfddf3530a
Techcable <Techcable@techcable.net>
2016-05-16 09:40:35 -0700

    Update to 1.9.4

---
+++ CombatTagPlusCompat-v1_9_R2/src/main/java/net/minelink/ctplus/compat/v1_9_R2/NpcPlayer.java
@@ -0,0 +1,48 @@
+package net.minelink.ctplus.compat.v1_9_R2;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import net.minecraft.server.v1_9_R2.EntityPlayer;
+import net.minecraft.server.v1_9_R2.MinecraftServer;
+import net.minecraft.server.v1_9_R2.PlayerInteractManager;
+import net.minecraft.server.v1_9_R2.WorldServer;
+import net.minelink.ctplus.compat.api.NpcIdentity;
+import net.minelink.ctplus.compat.api.NpcNameGeneratorFactory;
+import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+import java.util.UUID;
+
+public final class NpcPlayer extends EntityPlayer {
+
+    private NpcIdentity identity;
+
+    private NpcPlayer(MinecraftServer minecraftserver, WorldServer worldserver, GameProfile gameprofile, PlayerInteractManager playerinteractmanager) {
+        super(minecraftserver, worldserver, gameprofile, playerinteractmanager);
+    }
+
+    public NpcIdentity getNpcIdentity() {
+        return identity;
+    }
+
+    public static NpcPlayer valueOf(Player player) {
+        MinecraftServer minecraftServer = MinecraftServer.getServer();
+        WorldServer worldServer = ((CraftWorld) player.getWorld()).getHandle();
+        PlayerInteractManager playerInteractManager = new PlayerInteractManager(worldServer);
+        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), NpcNameGeneratorFactory.getNameGenerator().generate(player));
+
+        for (Map.Entry<String, Property> entry: ((CraftPlayer) player).getProfile().getProperties().entries()) {
+            gameProfile.getProperties().put(entry.getKey(), entry.getValue());
+        }
+
+        NpcPlayer npcPlayer = new NpcPlayer(minecraftServer, worldServer, gameProfile, playerInteractManager);
+        npcPlayer.identity = new NpcIdentity(player);
+
+        new NpcPlayerConnection(npcPlayer);
+
+        return npcPlayer;
+    }
+
+}

fd88a9471b9d887283f2e2193de51b058880c426
Maxopoly <maxopoly@civcraft.co>
2016-06-10 22:06:20 +0200

    Update to 1.10
    Merges #72

---
+++ CombatTagPlusCompat-v1_10_R1/src/main/java/net/minelink/ctplus/compat/v1_10_R1/NpcNetworkManager.java
@@ -0,0 +1,100 @@
+package net.minelink.ctplus.compat.v1_10_R1;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.concurrent.GenericFutureListener;
+import net.minecraft.server.v1_10_R1.EnumProtocol;
+import net.minecraft.server.v1_10_R1.EnumProtocolDirection;
+import net.minecraft.server.v1_10_R1.NetworkManager;
+import net.minecraft.server.v1_10_R1.Packet;
+import net.minecraft.server.v1_10_R1.PacketListener;
+
+import javax.crypto.SecretKey;
+import java.net.SocketAddress;
+
+public final class NpcNetworkManager extends NetworkManager {
+
+    public NpcNetworkManager() {
+        super(EnumProtocolDirection.SERVERBOUND);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext channelhandlercontext) throws Exception {
+
+    }
+
+    @Override
+    public void setProtocol(EnumProtocol enumprotocol) {
+
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext channelhandlercontext) {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext channelhandlercontext, Throwable throwable) {
+
+    }
+
+    @Override
+    protected void a(ChannelHandlerContext channelhandlercontext, Packet packet) {
+
+    }
+
+    @Override
+    public void setPacketListener(PacketListener packetlistener) {
+
+    }
+
+    @Override
+    public void sendPacket(Packet packet) {
+
+    }
+
+    @Override
+    public void sendPacket(Packet packet, GenericFutureListener genericfuturelistener, GenericFutureListener... agenericfuturelistener) {
+
+    }
+
+    @Override
+    public SocketAddress getSocketAddress() {
+        return new SocketAddress() {};
+    }
+
+    @Override
+    public boolean isLocal() {
+        return false;
+    }
+
+    @Override
+    public void a(SecretKey secretkey) {
+
+    }
+
+    @Override
+    public boolean isConnected() {
+        return true;
+    }
+
+    @Override
+    public void stopReading() {
+
+    }
+
+    @Override
+    public void setCompressionLevel(int i) {
+
+    }
+
+    @Override
+    public void handleDisconnection() {
+
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelhandlercontext, Packet object) throws Exception {
+
+    }
+
+}

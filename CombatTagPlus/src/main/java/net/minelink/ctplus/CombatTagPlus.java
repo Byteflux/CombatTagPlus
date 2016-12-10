b958a7a714ee1751c330d7e0b1e79f3cd5367fb5
Techcable <Techcable@techcable.net>
2016-12-10 11:25:07 -0700

    Add an untag command
    Effectively fixes #85, since server owners can now customize untag logic
    however they want.

--- CombatTagPlus/src/main/java/net/minelink/ctplus/CombatTagPlus.java
+++ CombatTagPlus/src/main/java/net/minelink/ctplus/CombatTagPlus.java
@@ -285,6 +285,26 @@ public final class CombatTagPlus extends JavaPlugin {
 
             // Attempt to start a new logout task
             SafeLogoutTask.run(this, player);
+        } else if (cmd.getName().equals("ctplusuntag")) {
+            if (!(sender instanceof Player)) return false;
+
+            if (args.length < 1) {
+                sender.sendMessage(RED + " Please specify a player to untag");
+                return true;
+            }
+
+            @SuppressWarnings("deprecation")
+            Player player = getServer().getPlayer(args[0]);
+            if (player == null || getNpcPlayerHelper().isNpc(player)) {
+                sender.sendMessage(RED + args[0] + " is not currently online!");
+                return true;
+            }
+            UUID uniqueId = player.getUniqueId();
+            if (getTagManager().untag(uniqueId)) {
+                sender.sendMessage(GREEN + "Successfully untagged " + player.getName() + ".");
+            } else {
+                sender.sendMessage(GREEN + player.getName() + " is already untagged.");
+            }
         }
 
         return true;

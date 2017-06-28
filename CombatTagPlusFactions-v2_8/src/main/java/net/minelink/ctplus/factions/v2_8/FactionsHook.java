0fd63b7b60f04af0f7728dbde26807a2dd1a59a4
TheSnoozer <s.lange@alumni.tu-berlin.de>
2017-06-29 01:53:53 +0200

    https://github.com/MinelinkNetwork/CombatTagPlus/issues/110: Adding support for faction versions 2.8, 2.9, 2.10, 2.11 and 2.12

---
+++ CombatTagPlusFactions-v2_8/src/main/java/net/minelink/ctplus/factions/v2_8/FactionsHook.java
@@ -0,0 +1,19 @@
+package net.minelink.ctplus.factions.v2_8;
+
+import com.massivecraft.factions.entity.BoardColl;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MFlag;
+import com.massivecraft.massivecore.ps.PS;
+import net.minelink.ctplus.hook.Hook;
+import org.bukkit.Location;
+
+public class FactionsHook implements Hook {
+
+    @Override
+    public boolean isPvpEnabledAt(Location location) {
+        PS ps = PS.valueOf(location);
+        Faction faction = BoardColl.get().getFactionAt(ps);
+        return faction.getFlag(MFlag.ID_PVP);
+    }
+
+}
